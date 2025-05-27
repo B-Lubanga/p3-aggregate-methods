776c9919926a024da25109838f4b50befb903b53
{B-Lubanga} <{Lubangabenjamin@gmail.com}>
2025-05-28 01:19:45 +0300

    aggregate method that counts the number of courses a Student

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {} 
 
     def enroll(self, course):
         if isinstance(course, Course):
@@ -15,9 +17,23 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        return len(self._enrollments)
+
+    def add_grade(self, enrollment, grade):
+        if isinstance(enrollment, Enrollment):
+            self._grades[enrollment] = grade
+        else:
+            raise TypeError("grade must be linked to an Enrollment instance")
+
+    def aggregate_average_grade(self):
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        return total_grades / num_courses if num_courses else 0
+
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -33,7 +49,7 @@ class Course:
 
 class Enrollment:
     all = []
-    
+
     def __init__(self, student, course):
         if isinstance(student, Student) and isinstance(course, Course):
             self.student = student
@@ -45,3 +61,11 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
